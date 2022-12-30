dc2c50574502de1f0ced884552d0f04191d5ed9e
mazenamr <14795777+mazenamr@users.noreply.github.com>
2022-12-30 19:03:43 +0200

    Better Recognition

--- recognition/fisher.py
+++ recognition/fisher.py
@@ -1,3 +1,4 @@
+from typing import Any
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -8,45 +9,25 @@ class fisherfaces:
     def __init__(self) -> None:
         self.scaler = StandardScaler()
         self.pca = PCA(n_components=0.95, svd_solver="full")
-        self.lda = LinearDiscriminantAnalysis(solver="eigen")
-
-    def fit_transform(self, X: np.ndarray, y: np.ndarray) -> None:
-        # Standardize the data
-        X_standardized = self.scaler.fit_transform(X)
-
-        # Perform PCA to reduce the dimensionality of the data
-        X_pca = self.pca.fit_transform(X_standardized)
-
-        # Perform Linear Discriminant Analysis on the reduced data
-        return self.lda.fit_transform(X_pca, y)
-
-
-class fisherfaces2:
-    def __init__(self) -> None:
-        self.scaler = StandardScaler()
-        self.pca = PCA(n_components=10, svd_solver="full")
-        self.lda = LinearDiscriminantAnalysis(solver="eigen", shrinkage="auto")
+        self.lda = LinearDiscriminantAnalysis(solver="svd")
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        # Histogram Equalization
+        # X_equalized = histogram_equalization(X)
+
         # Standardize the data
+        # X_standardized = self.scaler.fit_transform(X_equalized)
         X_standardized = self.scaler.fit_transform(X)
 
         # Perform PCA to reduce the dimensionality of the data
         X_pca = self.pca.fit_transform(X_standardized)
 
         # Perform Linear Discriminant Analysis on the reduced data
-        return self.lda.fit_transform(X_pca, y)
+        self.lda.fit(X_pca, y)
 
-    def project(self, X: np.ndarray) -> np.ndarray:
+    def predict(self, X: np.ndarray) -> Any:
+        # X_equalized = histogram_equalization(X)
+        # X_standardized = self.scaler.transform(X_equalized)
         X_standardized = self.scaler.transform(X)
         X_pca = self.pca.transform(X_standardized)
-        print(X_pca.shape)
-        print(self.eigenvectors.shape)
-        return X_pca @ self.eigenvectors
-
-    @property
-    def eigenvectors(self) -> np.ndarray:
-        print(f"lda: {self.lda.scalings_.shape}")
-        print(f"pca: {self.pca.components_.shape}")
-        print(f"dot: {(self.lda.scalings_ * self.pca.components_[:,:10]).shape}")
-        return self.lda.scalings_ * self.pca.components_[:, :10]
+        return self.lda.predict(X_pca)
--- recognition/helpers.py
+++ recognition/helpers.py
@@ -25,7 +25,7 @@ def detect_face(input_img):
     return image[y : y + w, x : x + h], faces[0]
 
 
-def prepare_data(path):
+def prepare_data(path, n=21, l=None):
     detected_faces = []
     face_labels = []
     image_dirs = os.listdir(path)
@@ -34,12 +34,12 @@ def prepare_data(path):
         images_path = path + "/" + dir_name
         images_names = os.listdir(images_path)
 
-        for image_name in images_names:
+        for image_name in images_names[:l]:
             image_path = images_path + "/" + image_name
             image = cv2.imread(image_path)
             face, _ = detect_face(image)
             if face is not None:
-                resized_face = cv2.resize(face, (21, 21), interpolation=cv2.INTER_AREA)
+                resized_face = cv2.resize(face, (n, n), interpolation=cv2.INTER_AREA)
                 detected_faces.append(resized_face)
                 face_labels.append(label)
 
@@ -55,38 +55,19 @@ def gaborize(images: np.ndarray) -> np.ndarray:
             for lamda in np.arange(0, np.pi, np.pi / 2):
                 for gamma in (0.05, 0.5):
                     kernel = cv2.getGaborKernel(
-                        (21, 21), sigma, theta, lamda, gamma, 0, ktype=cv2.CV_32F
+                        (7, 7), sigma, theta, lamda, gamma, 0, ktype=cv2.CV_32F
                     )
                     kernels.append(kernel)
+
     for image in images:
         d = image.shape[0]
         if len(image.shape) > 1:
             d *= image.shape[1]
-        gaborized = np.zeros(d * (len(kernels) + 1))
-        gaborized[0:d] = image.flatten()
+        gaborized = np.zeros(d * (len(kernels)))
+        # gaborized[0:d] = image.flatten()
         for i, kernel in enumerate(kernels):
             filtered = cv2.filter2D(image, cv2.CV_8UC3, kernel)
-            gaborized[d * (i + 1) : d * (i + 2)] = abs(filtered.flatten())
+            gaborized[d * (i + 0) : d * (i + 1)] = abs(filtered.flatten())
         result.append(gaborized)
+
     return np.array(result)
-
-
-def histogram_equalization(X: np.ndarray) -> np.ndarray:
-    # Convert the input array to a 3D array if it is not already
-    if len(X.shape) == 2:
-        X = X[:, :, np.newaxis]
-    elif len(X.shape) == 3 and X.shape[2] == 1:
-        X = X[:, :, 0]
-
-    # Perform histogram equalization on each channel of the input array
-    X_equalized = np.zeros(X.shape)
-    for i in range(X.shape[2]):
-        # Convert the channel to 8-bit single channel image before calling cv2.equalizeHist
-        X_8uc1 = X[:, :, i].astype(np.uint8)
-        X_equalized[:, :, i] = cv2.equalizeHist(X_8uc1)
-
-    # Convert the output array back to a 2D array if it is 3D with only one channel
-    if len(X_equalized.shape) == 3 and X_equalized.shape[2] == 1:
-        X_equalized = X_equalized[:, :, 0]
-
-    return X_equalized
