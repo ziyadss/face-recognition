7a0b8e0adfb9f3a72085e23b93350a99876f49ac
Ziyad Sameh <ziyad.ss@hotmail.com>
2022-12-31 02:36:10 +0200

    recognition

--- recognition/fisher.py
+++ recognition/fisher.py
@@ -1,5 +1,4 @@
 import pickle
-from typing import Any
 
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -9,30 +8,21 @@ from .constants import CLASSIFIER_PATH
 
 
 class FisherRecognizer:
-    def __init__(self, load=False) -> None:
-        if load:
-            self.load()
-        else:
-            self.scaler: StandardScaler = StandardScaler()
-            self.pca: PCA = PCA(n_components=0.95, svd_solver="full")
-            self.lda: LinearDiscriminantAnalysis = LinearDiscriminantAnalysis(
-                solver="svd"
-            )
+    def __init__(self):
+        self.scaler: StandardScaler = StandardScaler()
+        self.pca: PCA = PCA(n_components=0.95, svd_solver="full")
+        self.lda: LinearDiscriminantAnalysis = LinearDiscriminantAnalysis(solver="svd")
 
-    def load(self, path: str = CLASSIFIER_PATH) -> None:
+    def load(self, path: str = CLASSIFIER_PATH):
         with open(path, "rb") as fd:
             self.scaler, self.pca, self.lda = pickle.load(fd)
 
-    def dump(self, path: str = CLASSIFIER_PATH) -> None:
+    def dump(self, path: str = CLASSIFIER_PATH):
         with open(path, "wb") as fd:
             pickle.dump((self.scaler, self.pca, self.lda), fd)
 
-    def fit(self, X: list, y: list) -> None:
-        # Histogram Equalization
-        # X_equalized = histogram_equalization(X)
-
+    def fit(self, X: list, y: list):
         # Standardize the data
-        # X_standardized = self.scaler.fit_transform(X_equalized)
         X_standardized = self.scaler.fit_transform(X)
 
         # Perform PCA to reduce the dimensionality of the data
@@ -41,14 +31,12 @@ class FisherRecognizer:
         # Perform Linear Discriminant Analysis on the reduced data
         self.lda.fit(X_pca, y)
 
-    def predict(self, X: list) -> Any:
-        # X_equalized = histogram_equalization(X)
-        # X_standardized = self.scaler.transform(X_equalized)
+    def predict(self, X: list):
         X_standardized = self.scaler.transform(X)
         X_pca = self.pca.transform(X_standardized)
         return self.lda.predict(X_pca)
 
-    def score(self, X: list, y: list) -> Any:
+    def score(self, X: list, y: list):
         X_standardized = self.scaler.transform(X)
         X_pca = self.pca.transform(X_standardized)
         return self.lda.score(X_pca, y)
