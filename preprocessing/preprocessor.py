676b676abcddf67b3417f4bbd0b2f25ea52ac65d
Ziyad Sameh <ziyad.ss@hotmail.com>
2022-12-31 02:36:18 +0200

    preprocessing

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -1,19 +1,17 @@
+from collections.abc import Iterator
+
 import numpy as np
 from skimage import transform
 
-
-class PreprocessorConfiguration:
-    def __init__(self, output_shape: tuple[int, int]):
-        self.output_shape = output_shape
+from .constants import OUTPUT_SHAPE
 
 
 class Preprocessor:
-    def __init__(self, config: PreprocessorConfiguration):
-        self.config = config
+    def preprocess(
+        self, images: list[np.ndarray] | Iterator[np.ndarray]
+    ) -> list[np.ndarray]:
+        resized = (transform.resize(image, OUTPUT_SHAPE) for image in images)
 
-    def preprocess(self, images: list[np.ndarray]) -> list[np.ndarray]:
-        resized = [
-            transform.resize(image, self.config.output_shape) for image in images
-        ]
+        flattened = (image.flatten() for image in resized)
 
-        return resized
+        return list(flattened)
